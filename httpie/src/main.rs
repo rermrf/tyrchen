f98d18ee625d452ff4bf310d6cf5e9ba33f04910
emoji <emoji@emojideMBP.lan>
2023-10-09 22:30:41 +0800

    实现具体功能

--- httpie/src/main.rs
+++ httpie/src/main.rs
@@ -1,8 +1,9 @@
-use std::str::FromStr;
+use std::{str::FromStr, collections::HashMap};
 
 use clap::{Subcommand, Parser, arg};
-use anyhow::{Result, anyhow};
-use reqwest::Url;
+use anyhow::{Result, anyhow, Ok};
+use reqwest::{Url, Client, header::{HeaderMap, self}};
+// use serde::{Serialize, Deserialize};
 
 // 定义cli主程序
 #[derive(Parser, Debug)]
@@ -28,6 +29,13 @@ struct Get{
     url: String,
 }
 
+// get函数具体执行
+async fn get(client: Client, args: &Get) -> Result<()> {
+    let resp = client.get(&args.url).send().await?;
+    println!("{}", resp.text().await?);
+    Ok(())
+}
+
 // post子命令
 #[derive(Parser, Debug)]
 struct Post{
@@ -40,6 +48,16 @@ struct Post{
     body: Vec<KvPair>,
 }
 
+async fn post(client: Client, args: &Post) -> Result<()> {
+    let mut body: HashMap<String, String> = HashMap::new();
+    for pair in args.body.iter() {
+        body.insert(pair.key.clone(), pair.value.clone());
+    }
+    let resp = client.post(&args.url).json(&body).send().await?;
+    println!("{}", resp.text().await?);
+    Ok(())
+}
+
 // 验证输入的url是否合法
 fn parse_url(s: &str) -> Result<String> {
     let _url: Url = s.parse().expect("url 格式错误");
@@ -48,6 +66,7 @@ fn parse_url(s: &str) -> Result<String> {
 
 // 命令行中的 key=value，通过 parse_kv_pair 解析
 #[derive(Debug, Clone)]
+// #[derive(Serialize, Deserialize)]
 struct KvPair {
     key: String,
     value: String,
@@ -75,7 +94,25 @@ fn parse_kv_pair(s: &str) -> Result<KvPair> {
     Ok(s.parse()?)
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     let opts = Options::parse();
-    println!("{:?}", opts);
+
+    // 生成一个HTTP客户端
+    let mut headers = HeaderMap::new();
+    headers.insert("X-POWERED-BY", "Rust".parse()?);
+    headers.insert(header::USER_AGENT, "rermrf".parse()?);
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()?;
+    let result = match opts.subcmd {
+        SubCommand::Get(ref args) => {
+            get(client, args).await?
+        },
+        SubCommand::Post(ref args) => {
+            post(client, args).await?
+        }, 
+    };
+
+    Ok(result)
 }
