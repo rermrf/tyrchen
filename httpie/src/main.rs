d20f3ff2c5bd33ae29e6324b681bf8ec57b8afed
emoji <emoji@emojideMacBook-Pro.local>
2023-10-09 19:03:51 +0800

    first commint

--- httpie/src/main.rs
+++ httpie/src/main.rs
@@ -1,3 +1,30 @@
+use clap::{Subcommand, Parser};
+
+#[derive(Parser, Debug)]
+#[command(author, version)]
+struct Options {
+    #[command(subcommand)]
+    subcmd: SubCommand,
+}
+
+#[derive(Subcommand, Debug)]
+enum SubCommand{
+    Get(Get),
+    Post(POst),
+}
+
+#[derive(Parser, Debug)]
+struct Get{
+    url: String,
+}
+
+#[derive(Parser, Debug)]
+struct POst{
+    url: String,
+    body: Vec<String>,
+}
+
 fn main() {
+    
     println!("Hello, world!");
 }
