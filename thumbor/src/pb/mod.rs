180c1263f491380d5a780f325a47c95605a53107
rermrf <3219431643@qq.com>
2023-10-10 23:37:26 +0800

    继续学习陈老师的代码

--- thumbor/src/pb/mod.rs
+++ thumbor/src/pb/mod.rs
@@ -1,5 +1,6 @@
-use prost::Message;
 use base64::{engine::general_purpose, Engine};
+use photon_rs::transform::SamplingFilter;
+use prost::Message;
 
 // 声明abi.rs
 mod abi;
@@ -19,3 +20,47 @@ impl From<&ImageSpec> for String {
         general_purpose::URL_SAFE_NO_PAD.encode(data)
     }
 }
+
+// 让 ImageSpec 可以通过一个字符串创建。比如 s.parse().unwrap()
+impl TryFrom<&str> for ImageSpec {
+    type Error = anyhow::Error;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let data = general_purpose::URL_SAFE_NO_PAD.decode(value)?;
+        Ok(ImageSpec::decode(&data[..])?)
+    }
+}
+
+// 辅助函数，photon_rs 相应的方法里需要字符串
+impl filter::Filter {
+    pub fn to_str(&self) -> Option<&'static str> {
+        match self {
+            filter::Filter::Unspecified => None,
+            filter::Filter::Oceanic => Some("oceanic"),
+            filter::Filter::Islands => Some("islands"),
+            filter::Filter::Marine => Some("marine"),
+        }
+    }
+}
+
+// 在我们定义的 SampleFilter 和 photon_rs 的 SamplingFilter 间转换
+impl From<resize::SampleFilter> for SamplingFilter {
+    fn from(value: resize::SampleFilter) -> Self {
+        match value {
+            resize::SampleFilter::Undefined => SamplingFilter::Nearest,
+            resize::SampleFilter::Nearest => SamplingFilter::Nearest,
+            resize::SampleFilter::Triangle => SamplingFilter::Triangle,
+            resize::SampleFilter::CatmullRom => SamplingFilter::CatmullRom,
+            resize::SampleFilter::Gaussian => SamplingFilter::Gaussian,
+            resize::SampleFilter::Lanczos3 => SamplingFilter::Lanczos3,
+        }
+    }
+}
+
+// 提供一些辅助函数， 让创建一个 spec 的过程简单一些
+impl Spec {
+    
+}
+
+#[cfg(test)]
+mod tests {}
